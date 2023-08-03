e60c4dfda91ce178246ca249fb5fddcc590e735a
Dary <dady281100@gmail.com>
2023-08-02 23:39:16 -0500

    Screenshot working

--- webDownloader.py
+++ webDownloader.py
@@ -1,10 +1,18 @@
-import pyautogui
+from PIL import ImageGrab
 import keyboard
 
 def take_screenshot():
-    """Take a screenshot of the screen."""
+    """Take a screenshot of the selected region on the screen."""
     print("Click and drag to select the region for the screenshot.")
-    region = pyautogui.screenshot()
+    region = ImageGrab.grab(bbox=None)
+    return region
+
+def select_region(screenshot):
+    """Prompt the user to select a region by clicking and dragging on the image."""
+    image = screenshot.copy()
+    image.show()
+    print("Please click and drag to select the region. Press Enter when done.")
+    region = input("Press Enter after selecting the region.")
     return region
 
 def ask_questions():
@@ -14,14 +22,16 @@ def ask_questions():
     return module, lesson
 
 def main():
-    print("Press CTRL + S to take a screenshot.")
+    print("Press f5 to take a screenshot.")
     while True:
-        if keyboard.is_pressed('ctrl+s'):
+        if keyboard.is_pressed('f5'):
             screenshot = take_screenshot()
-            module, lesson = ask_questions()
-            filename = f"{module}_{lesson}.png"
-            screenshot.save(filename)
-            print(f"Screenshot saved as '{filename}'")
+            region = select_region(screenshot)
+            if region:
+                module, lesson = ask_questions()
+                filename = f"{module}_{lesson}.png"
+                screenshot.save(filename)
+                print(f"Screenshot saved as '{filename}'")
             break
 
 if __name__ == "__main__":
